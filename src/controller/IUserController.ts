94eadec8185ab1ca162aa9476a391f5a75abc12c
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-27 21:31:59 -0300

    feat(user): add hack to instantiate DAOMOngoose

--- src/controller/IUserController.ts
+++ src/controller/IUserController.ts
@@ -1,29 +1,28 @@
-// TODO: perguntar se isso tipar req e res causa acoplamento
-
-import User from "../persistence/User";
-import { TypedRequestBody, TypedResponse } from "../utils";
-
+// TODO: perguntar se isso de tipar req e res causa acoplamento
 // Porque o estamos expondo o express na criação desses DTOs
+
+import User from '../persistence/User';
+import { TypedRequestBody, TypedResponse } from '../utils';
+
 interface IUserController {
-    login(req: UserLoginRequest, res: UserLoginResponse): void;
-    registrar(req: UserRegistroRequest, res: UserLoginResponse): void;
+  login(req: UserLoginRequest, res: UserLoginResponse): void;
+  registrar(req: UserRegistroRequest, res: UserLoginResponse): void;
 }
 
 export type UserRegistroRequest = TypedRequestBody<{
-    user: User;
-    senha: string;
+  user: User;
+  senha: string;
 }>;
 export type UserRegistroResponse = TypedResponse<{
-    user: User | {};
-    msgCode: 200 | 500;
-    msg?: string;
+  user: User | {};
+  msgCode: 200 | 500;
+  msg?: string;
 }>;
-
 
 export type UserLoginRequest = TypedRequestBody<{ cpf: string; senha: string }>;
 export type UserLoginResponse = TypedResponse<{
-    user: User | {};
-    msgCode: 200 | 404 | 500;
-    msg?: string;
+  user: User | {};
+  msgCode: 200 | 404 | 500;
+  msg?: string;
 }>;
 export default IUserController;
--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,48 +1,40 @@
 import IUserController, {
-    UserLoginRequest,
-    UserLoginResponse,
-    UserRegistroRequest,
-    UserRegistroResponse,
+  UserLoginRequest,
+  UserLoginResponse,
+  UserRegistroRequest,
+  UserRegistroResponse,
 } from './IUserController';
+import IUserDAO from '../persistence/DAO/user/IUserDAO';
 import UserDAOMongoose from '../persistence/DAO/user/UserDAOMongoose';
 
-// TODO: Ver porque userDAO está undefined (comentário), mesmo ele sendo instanciado no construtor
-// Se eu instanciar ele aqui, ele funciona
-const userDAO = new UserDAOMongoose();
-
+// Config não funciona, por algum motivo só da pra acessar o userDAO se tiver em escopo geral
+const userDAO: IUserDAO = new UserDAOMongoose()
 class UserController implements IUserController {
-    // userDAO: IUserDAO;
+  async login(req: UserLoginRequest, res: UserLoginResponse) {
+    var { cpf } = req.query;
 
-    // constructor() {
-    // this.userDAO = new config.UserDAO();
-    // }
-
-    async login(req: UserLoginRequest, res: UserLoginResponse) {
-        var { cpf } = req.body;
-
-        try {
-            let user = await userDAO.buscarUsuario(cpf);
-            if (user != null) {
-                return res.json({ user, msgCode: 200 });
-            }
-            return res.json({ user: {}, msgCode: 404 });
-        } catch (error) {
-            return res.json({ user: {}, msgCode: 500, msg: error });
-        }
+    try {
+      let user = await userDAO.buscarUsuario(cpf);
+      if (user != null) {
+        return res.json({ user, msgCode: 200 });
+      }
+      return res.json({ user: {}, msgCode: 404 });
+    } catch (error) {
+      return res.json({ user: {}, msgCode: 500, msg: error });
     }
+  }
 
-    async registrar(req: UserRegistroRequest, res: UserRegistroResponse) {
-        var { user } = req.body;
+  async registrar(req: UserRegistroRequest, res: UserRegistroResponse) {
+    var { user } = req.body;
 
-        try {
-            await userDAO.criarUsuario(user);
-            return res.json({ user, msgCode: 200 });
-        } catch (error) {
-            console.log(error);
-
-            return res.json({ user: {}, msgCode: 500, msg: error });
-        }
+    try {
+      await userDAO.criarUsuario(user);
+      return res.json({ user, msgCode: 200 });
+    } catch (error) {
+      console.log(error);
+      return res.json({ user: {}, msgCode: 500, msg: error });
     }
+  }
 }
 
 export default UserController;
