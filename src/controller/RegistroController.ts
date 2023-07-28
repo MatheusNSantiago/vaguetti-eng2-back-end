4a17a5972f8b8f27554e4e0e9add799c8f1ae43b
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2023-07-27 21:31:24 -0300

    feat(registro): add hack to instanciate DAOmongoose

--- src/controller/RegistroController.ts
+++ src/controller/RegistroController.ts
@@ -1,73 +1,79 @@
-import RegistroDAOMongoose from '../persistence/DAO/registro/registroDAOMongoose';
+import config from '../config';
+import RegistroDAOMongoose from '../persistence/DAO/registro/RegistroDAOMongoose';
+import IRegistroDAO from '../persistence/DAO/registro/iRegistroDAO';
 import IRegistroController, {
-    AdicionarRegistroRequest,
-    AdicionarRegistroResponse,
-    AlterarRegistroRequest,
-    AlterarRegistroResponse,
-    BuscarRegistrosRequest,
-    BuscarRegistrosResponse,
-    ExcluirRegistroRequest,
-    ExcluirRegistroResponse,
+  AdicionarRegistroRequest,
+  AdicionarRegistroResponse,
+  AlterarRegistroRequest,
+  AlterarRegistroResponse,
+  BuscarRegistrosRequest,
+  BuscarRegistrosResponse,
+  ExcluirRegistroRequest,
+  ExcluirRegistroResponse,
 } from './IRegistroController';
 
-const registroDAO = new RegistroDAOMongoose();
-
 class RegistroController implements IRegistroController {
-    async buscarRegistros(
-        req: BuscarRegistrosRequest,
-        res: BuscarRegistrosResponse
-    ): Promise<void> {
-        const cpf = req.body.cpf;
+  registroDAO: IRegistroDAO;
 
-        try {
-            let registros = await registroDAO.buscarRegistros(cpf);
-            return res.json({ registros, msgCode: 200 });
-        } catch (error) {
-            return res.json({ registros: [], msgCode: 500, msg: error });
-        }
+  constructor() {
+    this.registroDAO = new RegistroDAOMongoose();
+  }
+
+  async buscarRegistros(
+    req: BuscarRegistrosRequest,
+    res: BuscarRegistrosResponse
+  ): Promise<void> {
+    const cpf = req.body.cpf;
+
+    try {
+      let registros = await this.registroDAO.buscarRegistros(cpf);
+      return res.json({ registros, msgCode: 200 });
+    } catch (error) {
+      return res.json({ registros: [], msgCode: 500, msg: error });
     }
+  }
 
-    async adicionarRegistro(
-        req: AdicionarRegistroRequest,
-        res: AdicionarRegistroResponse
-    ): Promise<void> {
-        const { cpf, medicao } = req.body;
+  async adicionarRegistro(
+    req: AdicionarRegistroRequest,
+    res: AdicionarRegistroResponse
+  ): Promise<void> {
+    const { cpf, medicao } = req.body;
 
-        try {
-            let registro = await registroDAO.salvarRegistro(cpf, medicao);
-            return res.json({ registro: registro, msgCode: 200 });
-        } catch (error) {
-            return res.json({ registro: {}, msgCode: 500, msg: error });
-        }
+    try {
+      let registro = await this.registroDAO.salvarRegistro(cpf, medicao);
+      return res.json({ registro: registro, msgCode: 200 });
+    } catch (error) {
+      return res.json({ registro: {}, msgCode: 500, msg: error });
     }
+  }
 
-    async excluirRegistro(
-        req: ExcluirRegistroRequest,
-        res: ExcluirRegistroResponse
-    ): Promise<void> {
-        const { cpf, idRegistro } = req.body;
+  async excluirRegistro(
+    req: ExcluirRegistroRequest,
+    res: ExcluirRegistroResponse
+  ): Promise<void> {
+    const { cpf, idRegistro } = req.body;
 
-        try {
-            await registroDAO.excluirRegistro(cpf, idRegistro);
-            return res.json({ msgCode: 200 });
-        } catch (error) {
-            return res.json({ msgCode: 500, msg: error });
-        }
+    try {
+      await this.registroDAO.excluirRegistro(cpf, idRegistro);
+      return res.json({ msgCode: 200 });
+    } catch (error) {
+      return res.json({ msgCode: 500, msg: error });
     }
+  }
 
-    async alterarRegistro(
-        req: AlterarRegistroRequest,
-        res: AlterarRegistroResponse
-    ): Promise<void> {
-        const { cpf, registro } = req.body;
+  async alterarRegistro(
+    req: AlterarRegistroRequest,
+    res: AlterarRegistroResponse
+  ): Promise<void> {
+    const { cpf, registro } = req.body;
 
-        try {
-            await registroDAO.alterarRegistro(cpf, registro);
-            return res.json({ registro: registro, msgCode: 200 });
-        } catch (error) {
-            return res.json({ registro: {}, msgCode: 500, msg: error });
-        }
+    try {
+      await this.registroDAO.alterarRegistro(cpf, registro);
+      return res.json({ registro: registro, msgCode: 200 });
+    } catch (error) {
+      return res.json({ registro: {}, msgCode: 500, msg: error });
     }
+  }
 }
 
 export default RegistroController;
--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 
 export interface TypedRequestBody<T> extends Request {
     body: T;
+    query: any;
 }
 
 export interface TypedResponse<T> extends Response {
